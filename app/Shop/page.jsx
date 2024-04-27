36b8da206c90ef7be01355bae56135af7084a3ac
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-27 14:15:27 -0400

    update shop page

--- app/Shop/page.jsx
+++ app/Shop/page.jsx
@@ -1,14 +1,18 @@
 
 import ShopProductlist from '../Componets/HomePage/ShopProductlist'
+import { createArray } from '../myCodes/Util'
 
 
 function Shop() {
     return (
-        <div className='flex min-h-screen flex-col bg-[#fff9ee]'>
-            <br />
-            <br />
-            <br />
-            <ShopProductlist />
+        <div className='flex min-h-screen flex-col bg-black'>
+            <div className='relative mt-24 center gap-4 lg:hover:scale-110 trans'>
+                {createArray(4).map(i => {
+                    return (<div key={i} className='h-12 w-12 bg-white rounded-full'>
+
+                    </div>)
+                })}
+            </div>
 
 
         </div>
