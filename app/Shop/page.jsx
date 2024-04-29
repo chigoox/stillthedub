7adc89fd9903c1bb79e49347c4ae0623dff8fdd1
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-29 13:33:08 -0400

    update

--- app/Shop/page.jsx
+++ app/Shop/page.jsx
@@ -14,7 +14,7 @@ function Shop() {
             <div className='relative mt-24 center gap-4 lg:hover:scale-110 trans overflow-y-scroll'>
                 {category.map(i => {
                     return (
-                        <div key={i} className='center-col'>
+                        <Button key={i} className='center-col rounded-t-full h-auto w-auto bg-transparent text-white'>
                             <div className='h-20 w-20 overflow-hidden  rounded-full'>
                                 <img className='h-full w-full object-cover' src={
                                     i == 'Drinks' ? 'https://www.instacart.com/image-server/386x386/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_01dd896b-371a-4d04-9ff6-cdfdd949886f.png' :
@@ -23,7 +23,7 @@ function Shop() {
                                                 'https://www.instacart.com/assets/domains/product-image/file/large_3a3749ea-89a5-41a3-a995-152d3b8cb2ce.jpg'} alt="" />
                             </div>
                             <h1>{i}</h1>
-                        </div>
+                        </Button>
                     )
                 })}
             </div>
@@ -39,7 +39,7 @@ function Shop() {
                 </div>
             </div>
             <div className='p-2 center text-black'>
-                <Input startContent={<AiOutlineSearch />} type='text' className='h-24 lg:w-1/2 px-2' label='test' labelPlacement='inside' />
+                <Input startContent={<AiOutlineSearch />} type='text' className='h-24 lg:w-1/2 px-2' label='Search' labelPlacement='inside' />
 
             </div>
             <div className='ITEMS center  h-auto w-full lg:w-3/4 p-2 mx-auto'>
